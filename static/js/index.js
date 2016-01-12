dcbddbbe1194457c7a46ebf6028e3f1f8c81aeb9
daleili <lidalei151781@gmail.com>
2016-01-12 13:31:44 +0100

    index.html remove useless navigation tags, index.js add axis
    index.html remove useless navigation tags, index.js add axis

--- static/js/index.js
+++ static/js/index.js
@@ -1,124 +1,121 @@
 /**
  * file name: index.js
- * time: Jan. 4, 2016
+ * time: Jan. 12, 2016
  * @author Dalei Li
  */
 
 $(function() { // executed after the html content is loaded completely
 	
-	// jQuery basics
-	// decalre variables
-	var $game = "Easy, let's rock!";
-	console.log($game);
+	/* ----Here begins the visualization code---- */
 	
-	var $arrayExample = ["Qiyue Wang", "Dalei Li"];
-	for(var i = 0; i < $arrayExample.length; ++i) {
-		// console.log(arrayExample[i] + ",");
-	}
-	
-	var $arrayWithNamesExample = {"Author 1": "Qiyue Wang", "Author 2": "Dalei Li"};
-	console.log($arrayWithNamesExample["Author 1"] + ", " + $arrayWithNamesExample["Author 2"]);
-	
-	// declare a function, function is the first class object in JS
-	function square(x) {
-		return x * x;
-	}
-	// an alternative way to declare a function
-	var _square = function(x) {
-		return x * x;
-	};
-	// functional forEach, First define an array
-	var arrayOfNumbers = [4, 5, 8, 9, 45];
-	arrayOfNumbers.forEach(function(d) {
-		console.log(d);
+	// auto adjust when resizing window
+	$(window).resize(function() {
+		var windowWidth = $(this).width(), windowHeight = $(this).height();		
+		var $canvas_svg = $("#canvas > svg"),
+			svgWidth = $canvas_svg.width(),
+			svgHeight = $canvas_svg.height();
+		$canvas_svg.height(Math.floor(svgWidth * windowHeight / windowWidth));
+		if(window.whiteWineInstances != null) {
+			// re-render
+			render(window.whiteWineInstances, true);
+		}
 	});
 	
-	// Here begins the visualization code!
-	// define margin and padding
-	var d3_svg = d3.select("#canvas").select("svg"),
-		svgWidth = d3_svg.style("width").replace("px", ""),
-		svgHeight = d3_svg.style("height").replace("px", ""),
-		margin = {top: 20, right: 20, bottom: 20, left: 20},
-		padding = {top: 60, right: 60, bottom: 60, left: 60},
-		innerWidth = svgWidth - margin.left - margin.right,
-		innerHeight = svgHeight - margin.top - margin.bottom,
-		width = innerWidth - padding.left - padding.right,
-		height = innerHeight - padding.top - padding.bottom;
-	
-	// set visible area, i.e., the container of visualized charts
-	var d3_svg_g = d3_svg.select("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-	
-	/*
-	// add elements
-	var d3_svg_g_text = d3_svg_g.append("text").text("SVG Learning").attr({"x": 20, "y": 10, "fill": "red", "stroke": "black", "stroke-width": 2})
-			.style("font-size", "2em");
-	var d3_svg_g_rect = d3_svg_g.append("rect").attr({"x": 100, "y": 100, "width": 100, "height": 100, "fill": "none", "stroke": "red", "stroke-width": 10, "rx": 10, "ry": 10})
-	var d3_svg_g_circle = d3_svg_g.append("circle").attr({"cx": 150, "cy": 150, "r": 50, "fill": "#9370DB", "stroke": "rgba(0, 0, 255, 0.8)"});
-	var d3_svg_g_line1 = d3_svg_g.append("line").attr({"x1": 100, "y1": 100, "x2": 200, "y2": 200, "stroke": "black", "stroke-width": 15});
-	var d3_svg_g_line2 = d3_svg_g.append("line").attr({"x1": 200, "y1": 100, "x2": 100, "y2": 200, "stroke": "black", "stroke-width": 15});
-	// M move to, L line to
-	var d3_svg_g_path1 = d3_svg_g.append("path").attr({"d": "M50 50 L50 250 M250 250 L250 50", "fill": "none", "stroke": "yellow", "stroke-width": 15});
-	// Z close the path
-	var d3_svg_g_path2 = d3_svg_g.append("path").attr({"d": "M25 25 L25 275 L275 275 L275 25Z", "fill": "none", "stroke": "yellow", "stroke-width": 15});	
-	
-	// d3 linear scale
-	var linearScale = d3.scale.linear()
-        .domain([0, 100])   // Data space
-        .range([300, 2800]); // Pixel space
-		
-	// d3 ordinal scale
-	var ordinalScale = d3.scale.ordinal()
-		.domain(["A", "B", "C", "D"])
-		.rangeRoundPoints([0, 100]);//.rangePoints([0, 100]);//.range(["Apple", "Banana", "Coconut", "D3"]);
-	
-	// D3 basics
-	// Update…
-	var d3_svg_g_paths = d3_svg_g.selectAll("path")
-		.data([10, 12, 8, 6, 4])
-		.attr("stroke-width", function(d) { return d + "px"; });
-	
-	// Enter handles added data only
-	setTimeout(
-		function(){d3_svg_g_paths.enter().append("path").attr({"d": function(d){return "M25 " + linearScale(d) + " L275 " + linearScale(d);}, "fill": "none", "stroke": "yellow", "stroke-width": function(d) { return d + "px"; }});}
-		, 1000);
-	// Exit…
-	d3_svg_g_paths.exit().remove();
-	*/
+	// define the global variable to store instances (dataset)
+	window.whiteWineInstances = null;
 	
 	// rendering function
-	function render(instances) {
+	function render(instances, isUpdate) {
+		// define margin and padding
+		var d3_svg = d3.select("#canvas").select("svg"),
+			svgWidth = d3_svg.style("width").replace("px", ""),
+			svgHeight = d3_svg.style("height").replace("px", ""),
+			margin = {top: 20, right: 20, bottom: 20, left: 20},
+			padding = {top: 60, right: 60, bottom: 60, left: 60},
+			innerWidth = svgWidth - margin.left - margin.right,
+			innerHeight = svgHeight - margin.top - margin.bottom,
+			width = innerWidth - padding.left - padding.right,
+			height = innerHeight - padding.top - padding.bottom;
+		
+		// set visible area, i.e., the container of visualized charts
+		var d3_svg_g = d3_svg.select("g").attr({"transform": "translate(" + margin.left + "," + margin.top + ")"});
+		
 		var attributes = Object.keys(instances[0]);
 		// alcohol (0) and pH (6) to predict quality (7)
 		var xAttr = "alcohol", yAttr = "pH", zAttr = "quality";
 		var rMin = 2, rMax = 20; // "r" stands for radius
-		// Update…
-		var d3_svg_g_circles = d3_svg_g.selectAll("circle")
-			.data(instances);
 		
-		var xLinearScale = d3.scale.linear()
-			.domain(d3.extent(instances, function(instance){return instance[xAttr];}))   // Data space
-			.range([0, width]); // Pixel space
-		
-		var yLinearScale = d3.scale.linear()
-			.domain(d3.extent(instances, function(instance){return instance["pH"];}))
-			.range([height, 0]);
-
+		// map original data to svg visible area, width and height, respectively
+		var xLinearScale = d3.scale.linear().range([0, width]); // Pixel space
+		var yLinearScale = d3.scale.linear().range([height, 0]);
+		// define axis
+		var xAxis = d3.svg.axis().scale(xLinearScale).orient("bottom");
+		var yAxis = d3.svg.axis().scale(yLinearScale).orient("left");
+		/*
 		var zLinearScale = d3.scale.linear()
 			.domain(d3.extent(instances, function(instance){return instance["quality"];}))
 			.range([rMin, rMax]);
-
+		*/
+		var zColorScale = d3.scale.category10();
+		
+		// Update...
+		var d3_svg_g_circles = d3_svg_g.selectAll("circle").data(instances);
+		
+		// scatter, quality = f(alcohol, pH)
+		xLinearScale.domain(d3.extent(instances, function(instance){return instance[xAttr];}));
+		yLinearScale.domain(d3.extent(instances, function(instance){return instance[yAttr];}));
+		
+		if(isUpdate) { // is update
+			d3_svg_g_circles.transition().duration(300)
+				.attr({"fill": function(instance){return zColorScale(instance[zAttr]);}, "fill-opacity": 0.8, "stroke": "none", "stroke-width": 4, "cx": function(instance){return xLinearScale(instance[xAttr]);}, "cy": function(instance){return yLinearScale(instance[yAttr]);}, "r": function(instance){return 5;}});
+			
+			// update axis
+			d3_svg_g.select(".xaxis").attr({"transform": "translate(0," + height + ")"})
+			.transition().duration(150).ease("sin-in-out").call(xAxis);
+			d3_svg_g.select(".yaxis").transition().duration(150).ease("sin-in-out").call(yAxis);
+		}
+		else { // first time rendering
+			// set axis, give it a class so it can be used
+			//to select only xaxis labels  below
+			var xAxisG = d3_svg_g.append("g")
+				.attr({"transform": "translate(0," + height + ")", "class": "xaxis"});
+			var yAxisG = d3_svg_g.append("g").attr({"class": "yaxis"});		
+			// draw axis
+			xAxisG.call(xAxis);
+			yAxisG.call(yAxis);
+		}
 		
 		// Enter handles added data only
-		d3_svg_g_circles.enter().append("circle").attr({"fill": "none", "stroke": "rgba(0, 255, 0, 0.5)", "stroke-width": 4, "cx": function(instance){return xLinearScale(instance[xAttr]);}, "cy": function(instance){return yLinearScale(instance[yAttr]);}, "r": function(instance){return zLinearScale(instance[zAttr]);}});
+		d3_svg_g_circles.enter().append("circle")
+			.attr({"fill": function(instance){return zColorScale(instance[zAttr]);}, "fill-opacity": 0.8, "stroke": "none", "stroke-width": 4, "cx": function(instance){return xLinearScale(instance[xAttr]);}, "cy": function(instance){return yLinearScale(instance[yAttr]);}, "r": function(instance){return 5;}});
+		/*
+		// scatter, quality = f(alcohol)
+		xLinearScale.domain(d3.extent(instances, function(instance){return instance[xAttr];}));   // Data space
+		yLinearScale.domain(d3.extent(instances, function(instance){return instance[zAttr];}));
+		if(isUpdate) { // is update
+			d3_svg_g_circles.transition().duration(300)
+				.attr({"fill": function(instance){return zColorScale(instance[zAttr]);}, "fill-opacity": 0.8, "stroke": "none", "stroke-width": 4, "cx": function(instance){return xLinearScale(instance[xAttr]);}, "cy": function(instance){return yLinearScale(instance[zAttr]);}, "r": 5});
+		}
+		*/
+		// Enter handles added data only
+		d3_svg_g_circles.enter().append("circle")
+			.attr({"fill": function(instance){return zColorScale(instance[zAttr]);}, "fill-opacity": 0.8, "stroke": "none", "stroke-width": 4, "cx": function(instance){return xLinearScale(instance[xAttr]);}, "cy": function(instance){return yLinearScale(instance[zAttr]);}, "r": 5});
+		
 		// Exit…
 		d3_svg_g_circles.exit().remove();
 		
+		/*
 		// manully visualize, not applicable
 		instances.forEach(function(instance) {
 			
-		});
+		});*/
 	}
 	
+	// read CVS files
+	var d3_whiteWine = d3.csv("/static/dataset/wine/wine_white.csv", d3_type, function(instances) {
+		window.whiteWineInstances = instances;
+		render(window.whiteWineInstances, false);
+	});
 	// define the function to parse cvs attributes
 	function d3_type(instance) {
 		for (var attribute in instance) {
@@ -126,8 +123,7 @@ $(function() { // executed after the html content is loaded completely
 		}
 		return instance;
 	}
-	
-	var d3_whiteWine = d3.csv("/static/dataset/wine/wine_white.csv", d3_type, render);
+	/* ----Here ends the visualization code---- */
 	
 	// don't have to change
 	// scroll window to hide or show backToTop button
