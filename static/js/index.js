755f2fe886052d19d02f29895c0d72033ac19940
lukewang7 <lukewang.729@gmail.com>
2016-01-26 15:19:10 +0100

    radar chart and table plot

--- static/js/index.js
+++ static/js/index.js
@@ -187,13 +187,169 @@ $(function() { // executed after the HTML content is loaded completely
 		*/
 	}
 	
-	// read CVS files
+	function render_luc(instances, isUpdate) {
+		var w = 700,
+			h = 700;
+	
+		var colorscale = d3.scale.category10();
+		
+		//Legend titles
+		var LegendOptions = ['WhiteWine0001'];
+		
+		//Data
+		var d = 
+				[
+					[
+						{axis:"Fixed acidity",value:instances[0]["fixed acidity"]/d3.max(instances, function(instance){return instance["fixed acidity"]})},
+						{axis:"Volatile acidity",value:instances[0]["volatile acidity"]/d3.max(instances, function(instance){return instance["volatile acidity"]})},
+						{axis:"Citric acid",value:instances[0]["citric acid"]/d3.max(instances, function(instance){return instance["citric acid"]})},
+						{axis:"Residual sugar",value:instances[0]["residual sugar"]/d3.max(instances, function(instance){return instance["residual sugar"]})},
+						{axis:"Chlorides",value:instances[0]["chlorides"]/d3.max(instances, function(instance){return instance["chlorides"]})},
+						{axis:"Free sulfur dioxide",value:instances[0]["free sulfur dioxide"]/d3.max(instances, function(instance){return instance["free sulfur dioxide"]})},
+						{axis:"Total sulfur dioxide",value:instances[0]["total sulfur dioxide"]/d3.max(instances, function(instance){return instance["total sulfur dioxide"]})},
+						{axis:"Density",value:instances[0]["density"]/d3.max(instances, function(instance){return instance["density"]})},
+						{axis:"pH",value:instances[0]["pH"]/d3.max(instances, function(instance){return instance["pH"]})},
+						{axis:"Sulphates",value:instances[0]["sulphates"]/d3.max(instances, function(instance){return instance["sulphates"]})},
+						{axis:"Alcohol",value:instances[0]["alcohol"]/d3.max(instances, function(instance){return instance["alcohol"]})},
+					]
+				];
+				
+		//Options for the Radar chart, other than default
+		var mycfg = {
+		  w: w,
+		  h: h,
+		  maxValue: 1.0,
+		  levels: 6,
+		  ExtraWidthX: 300
+		}
+		
+		//Call function to draw the Radar chart
+		//Will expect that data is in %'s
+		RadarChart.draw("#chart", d, mycfg);
+		
+		////////////////////////////////////////////
+		/////////// Initiate legend ////////////////
+		////////////////////////////////////////////
+		
+		var svg = d3.select('#body')
+			.selectAll('svg')
+			.append('svg')
+			.attr({"width": w+500,"height": h})
+		
+		//Create the title for the legend
+		var text = svg.append("text")
+			.attr({"class": "title",'transform': 'translate(90,0)',"x": w - 100,"y": 20,"font-size": "16px","fill": "#404040"})
+			.text('The attributes of the wine "whitewine0001"');
+				
+		//Initiate Legend	
+		var legend = svg.append("g")
+			.attr({"class": "legend","height": 100,"width": 200,'transform':'translate(90,20)'})
+			;
+			//Create colour squares
+			legend.selectAll('rect')
+			  .data(LegendOptions)
+			  .enter()
+			  .append("rect")
+			  .attr({"x": w - 65,"y":function(d, i){ return i * 20+10;},"width":10,"height":10})
+			  .style("fill", function(d, i){ return colorscale(i);})
+			  ;
+			//Create text next to squares
+			legend.selectAll('text')
+			  .data(LegendOptions)
+			  .enter()
+			  .append("text")
+			  .attr({"x": w - 52, "y": function(d, i){ return i * 20 + 9+10;},"font-size": "11px","fill": "#737373"})
+			  .text(function(d) { return d; })
+			  ;	
+	
+	}
+	
+	function transform(attrName,data) {
+	    d3.select("tbody").selectAll("tr").remove();
+	
+		// Header
+	    var th = d3.select("thead").selectAll("th")
+	            .data(jsonToArray(data[0]))
+	          	.enter().append("th")
+	          	.on("click", function(d){transform(d[0], data);})
+	            .text(function(d) { return d[0]; });
+	
+		// Rows	
+	    var tr = d3.select("tbody").selectAll("tr")
+	    	.data(data)
+			.enter().append("tr")
+			// .sort(function (a, b) { return a - b});
+			.sort(function (a, b) { return a == null || b == null ? 0 : Compare(a[attrName], b[attrName]);});
+	
+		// Cells
+	    var td = tr.selectAll("td")
+	            .data(function(d) { return jsonToArray(d); })
+	        	.enter().append("td")
+	            .text(function(d) { return d[1]; });
+
+	}
+	
+	
+	function Compare(a, b) {
+
+		if(!isNaN(parseFloat(a)) && !isNaN(parseFloat(b))){
+
+			return parseFloat(a) > parseFloat(b) ? 1 : parseFloat(a)==parseFloat(b) ? 0 : -1;
+			
+		}else{
+			a = a.toLowerCase();
+			b = b.toLowerCase();
+			return a > b ? 1 : a == b ? 0 : -1;//for string comparison
+		}
+		
+	}
+	function jsonKeyValueToArray(k, v) {return [k, v];}
+	
+	function jsonToArray(json) {
+		    var ret = new Array();
+		    var key;
+		    for (key in json) {
+		        if (json.hasOwnProperty(key)) {
+		            ret.push(jsonKeyValueToArray(key, json[key]));
+		        }
+		    }
+		    return ret;
+		}
+	
+	function render_table(instances, isUpdate) {
+			
+		// var i = 0, data = [
+	 //   {ID: String(i++), age: "25", name: "luke", time: "1.11.2011"},
+	 //   {ID: String(i++), age: "30", name: "Thor", time: "1.11.2011"},
+	 //   {ID: String(i++), age: "5", name: "Xiaolei ", time: "1.11.2011"},
+	 //   {ID: String(i++), age: "22", name: "f(x,y)", time: "2.11.2011"},
+	 //   {ID: String(i++), age: "18", name: "C2D2", time: "5.11.2011"}
+		// ];
+		var i = 0, data = [];
+		var len = instances.length;
+		console.log(len);
+		for(;i < len; i++){
+			var str = "" + parseFloat(i+1);
+			var pad = "0000";
+			var ans = pad.substring(0, pad.length - str.length) + str;
+			data[i] = {"ID": "White_Wine" + ans,"Fixed Acidity" : instances[i]["fixed acidity"] , "Volatile Acidity" : instances[i]["volatile acidity"],"Citric Acid":instances[i]["citric acid"],"Residual Sugar":instances[i]["residual sugar"],"Chlorides":instances[i]["chlorides"],"Free Sulfur Dioxide":instances[i]["free sulfur dioxide"],"Total Sulfur Dioxide":instances[i]["total sulfur dioxide"],"Density":instances[i]["density"],"PH":instances[i]["pH"],"Sulphates":instances[i]["sulphates"],"Alcohol":instances[i]["alcohol"],"Quality":instances[i]["quality"]}
+		
+			
+		}
+		
+		transform("ID",data);
+		
+		}
+	// read CSV files
 	var d3_whiteWine = d3.csv("/static/dataset/wine/wine_white.csv", d3_type, function(error, instances) {
 		if (error){
 			throw error;
 		}
 		window.whiteWineInstances = instances;
-		render(window.whiteWineInstances, false);
+		// console.log(window.whiteWineInstances[0]);
+		// render(window.whiteWineInstances, false);
+		// render_luc(window.whiteWineInstances,false);
+		render_table(window.whiteWineInstances,false);
 	});
 	
 	/*
@@ -206,6 +362,7 @@ $(function() { // executed after the HTML content is loaded completely
 		}
 		return instance;
 	}
+	
 	/* ----Here ends the visualization code---- */
 	
 	// don't have to change
